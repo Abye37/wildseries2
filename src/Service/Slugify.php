35585060c88332b267c86fcdbec4a56620932129
Mayte <malapert.mayte@gmail.com>
2020-01-06 11:55:31 +0100

    debut de quête 16 par erreur sur mauvaise branche

---
+++ src/Service/Slugify.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Service;
+
+class Slugify
+{
+    private $slug;
+    public static function generate(string $input) : string
+    {
+        // replace non letter or digits by -
+        $input = preg_replace('~[^\pL\d]+~u', '-',  $input);
+        // transliterate
+        $input = preg_replace('~[^-\w]+~', '',  $input);
+        // trim
+        $input = trim( $input, '-');   
+        // lowercase
+        $input = strtolower( $input);
+        if (empty( $input)) {
+            return 'n-a';
+        }
+        return  $input;
+    }
+}
